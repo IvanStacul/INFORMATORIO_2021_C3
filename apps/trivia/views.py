a1df29797c5f7dcba607dadfd4fac6d455fd3395
IvanStacul <stacul.ivan98@gmail.com>
2021-08-25 17:40:08 -0300

    refactorizacion de codigo

--- apps/trivia/views.py
+++ apps/trivia/views.py
@@ -1,4 +1,5 @@
 # Django
+from django.db.utils import IntegrityError
 from django.utils import timezone
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
@@ -11,12 +12,9 @@ from .models import Category, Game, Level, Question, Option, Quiz
 # Exceptions
 from django.core.exceptions import ObjectDoesNotExist
 
+
 @login_required
 def play(request: HttpRequest, trivia: int):
-
-    # si el usuario no esta logueado se redirecciona al login
-    if not request.user.is_authenticated:
-        return redirect('login')
 
     # verifica si existe el juego que nos piden
     try:
@@ -36,13 +34,16 @@ def play(request: HttpRequest, trivia: int):
             }
         )
 
+    # progreso del juego
+    game = Game.objects.filter(quiz_id=trivia.pk)
+
     # obtenemos la primer pregunta no respondida del juego en progreso
-    progress = Game.objects.filter(quiz_id=trivia.pk, answered=False).first()
-    
+    progress = game.filter(answered=False).first()
+
     # luego extraemos las opciones a esa pregunta
     options = Option.objects.filter(
         question_id=progress.question.id,
-        level_id=progress.quiz.level.id
+        level_id=trivia.level_id
     )
 
     # si ya se envio una respuesta a la pregunta
@@ -52,35 +53,30 @@ def play(request: HttpRequest, trivia: int):
                 pk=request.POST.get('answer')
             )
         except ObjectDoesNotExist:
-            answer = Game.objects.get(  # pregunta que se respondio
+            answer = game.get(  # pregunta que se respondio
                 question_id=request.POST.get('question_id'),
-                quiz_id=trivia.pk
             )
             answer.answered = True  # marcar como respondida la pregunta
             answer.isCorrect = False  # marcar como erronea la pregunta
             answer.save()  # guardar resultados de la pregunta
-            
-            game = Game.objects.filter(quiz_id=trivia.pk)
 
             # si ya no existen mas preguntas por responder
             if not game.filter(answered=False).exists():
-                quiz = Quiz.objects.get(pk=trivia.pk)  # obtener el quiz actual
-                quiz.score = len(game.filter(isCorrect=True)) * 20
-                quiz.end = timezone.now()  # guardar el tiempo de fin del quiz
-                quiz.save()  # guardar resultados
-            
+                trivia.score = len(game.filter(isCorrect=True)) * 20
+                trivia.end = timezone.now()  # guardar el tiempo de fin del quiz
+                trivia.save()  # guardar resultados
+
             return render(
-                    request=request,
-                    template_name='trivia/false.html',
-                    context = {
-                        'description': 'no seleccionaste nignuna opcion',
-                        'trivia': trivia,
-                    }
-                )
-        
-        answer = Game.objects.get(  # pregunta que se respondio
+                request=request,
+                template_name='trivia/false.html',
+                context={
+                    'description': 'no seleccionaste nignuna opcion',
+                    'trivia': trivia,
+                }
+            )
+
+        answer = game.get(  # pregunta que se respondio
             question_id=request.POST.get('question_id'),
-            quiz_id=trivia.pk
         )
 
         answer.answered = True  # marcar como respondida la pregunta
@@ -102,14 +98,11 @@ def play(request: HttpRequest, trivia: int):
 
         answer.save()  # guardar resultados de la pregunta
 
-        game = Game.objects.filter(quiz_id=trivia.pk)
-
         # si ya no existen mas preguntas por responder
         if not game.filter(answered=False).exists():
-            quiz = Quiz.objects.get(pk=trivia.pk)  # obtener el quiz actual
-            quiz.score = len(game.filter(isCorrect=True)) * 20
-            quiz.end = timezone.now()  # guardar el tiempo de fin del quiz
-            quiz.save()  # guardar resultados
+            trivia.score = len(game.filter(isCorrect=True)) * 20
+            trivia.end = timezone.now()  # guardar el tiempo de fin del quiz
+            trivia.save()  # guardar resultados
 
         # renderea la pagina de resultado de la opcion elegida
         return render(
@@ -168,14 +161,31 @@ def config(request: HttpRequest):
                 }
             )
 
-        # crear un juego
-        quiz = Quiz.objects.create(
-            score=0,
-            category_id=category,
-            level_id=level,
-            player_id=request.user.player.id,
-            user_id=request.user.id,
-        )
+        try:
+            # crear un juego
+            quiz = Quiz.objects.create(
+                score=0,
+                category_id=category,
+                level_id=level,
+                player_id=request.user.player.id,
+                user_id=request.user.id,
+            )
+        except IntegrityError:
+            return render(
+                request=request,
+                template_name='trivia/error.html',
+                context={
+                    'description': 'No se pudo crear la partida porque no se configuro un nivel o categoría.'
+                }
+            )
+        except ValueError:
+            return render(
+                request=request,
+                template_name='trivia/error.html',
+                context={
+                    'description': 'No se pudo crear la partida porque el valor de nivel o categoría elegido es incorrecto.'
+                }
+            )
 
         # guardar el progreso del juego
         for question in questions:
