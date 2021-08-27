891032685a6e7df60c7567cd4f4bf2485d2cd88f
IvanStacul <stacul.ivan98@gmail.com>
2021-08-27 10:30:05 -0300

    urls de las aplicaciones cambiadas

--- chacu_py/urls.py
+++ chacu_py/urls.py
@@ -4,7 +4,7 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('jugador/', include('apps.players.urls')),
+    path('trivia/', include('apps.trivia.urls')),
     path('', include('apps.core.urls')),
-    path('', include('apps.players.urls')),
-    path('', include('apps.trivia.urls')),
 ]
