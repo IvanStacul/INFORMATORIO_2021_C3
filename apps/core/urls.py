7921f3a11ecd158da841c5297a8a0587c77c976d
IvanStacul <stacul.ivan98@gmail.com>
2021-08-31 22:03:28 -0300

    nuevas funcionalidades agregadas al website
    - panel de administracion
    - crud de preguntas, opciones, niveles y categorias
    - login con el nuevo frontend
    - prueba inicial de graficos estadisticos

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -7,4 +7,20 @@ urlpatterns = [
     path('nosotros', views.us, name='us'),
     path('ranking', views.ranking, name='ranking'),
     path('contacto', views.contact, name='contact'),
+    path('charts', views.chart_report, name='charts'),
+    path('data', views.pivot_data, name='pivot_data'),
+    path('panel', views.admin_panel, name='panel'),
+    path('panel/trivia/configuracion', views.trivia_config, name='trivia_config'),
+    path('panel/trivia/preguntas', views.trivia_questions, name='trivia_questions'),
+    path(
+        'panel/trivia/preguntas/<int:question_id>',
+        views.trivia_question_edit,
+        name='trivia_questions_edit'
+    ),
+    path('panel/trivia/opciones', views.trivia_options, name='trivia_options'),
+    path(
+        'panel/trivia/opciones/<int:option_id>',
+        views.trivia_option_edit,
+        name='trivia_options_edit'
+    ),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,9 +1,13 @@
 # Django
-from django.http.request import HttpRequest
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.core import serializers
 
 # Models
-from apps.trivia.models import Quiz
+from apps.trivia.models import Category, Level, Option, Question, Quiz
+from apps.players.models import Player
+
 
 def home(request: HttpRequest):
     return render(request, 'index.html')
@@ -23,6 +27,171 @@ def contact(request):
 
 def ranking(request: HttpRequest):
 
-    quizzes = Quiz.objects.raw('SELECT MAX(score), * FROM trivia_quiz GROUP BY player_id')
+    quizzes = Quiz.objects.raw(
+        'SELECT MAX(score), * FROM trivia_quiz GROUP BY player_id')
     quizzes = sorted(quizzes, key=lambda q: (q.score, q.time()), reverse=False)
     return render(request, 'core/ranking.html', context={'quizzes': quizzes})
+
+
+def chart_report(request: HttpRequest):
+    return render(request, 'core/charts.html')
+
+
+def pivot_data(request: HttpRequest):
+    dataset = Quiz.objects.all()
+    data = serializers.serialize('json', dataset)
+    return JsonResponse(data, safe=False)
+
+@login_required
+def admin_panel(request: HttpRequest):
+    context = {
+        'players_count': Player.objects.count(),
+        'quiz_count': Quiz.objects.count(),
+    }
+    return render(request, 'core/admin/panel.html', context)
+
+@login_required
+def trivia_config(request: HttpRequest):
+    context = {
+        'status': '',
+        'error': '',
+        'levels': Level.objects.all(),
+        'categories': Category.objects.all(),
+    }
+    if request.method == 'POST':
+        category = request.POST.get('category')
+        category_id = request.POST.get('category_id')
+        level = request.POST.get('level')
+        level_id = request.POST.get('level_id')
+        if category:
+            Category.objects.create(name=category)
+            context['status'] = 'Categoría creada con éxito'
+        if category_id:
+            Category.objects.filter(pk=category_id).delete()
+        if level:
+            Level.objects.create(name=level)
+            context['status'] = 'Nivel creada con éxito'
+        if level_id:
+            Level.objects.filter(pk=level_id).delete()
+        return redirect('trivia_config')
+    return render(request, 'core/admin/configuration.html', context)
+
+@login_required
+def trivia_questions(request: HttpRequest):
+    context = {
+        'status': '',
+        'error': '',
+        'levels': Level.objects.all(),
+        'categories': Category.objects.all(),
+        'questions': Question.objects.all(),
+    }
+    if request.method == 'POST':
+        question_id = request.POST.get('question_id')
+
+        data = {
+            'level_id': request.POST.get('level_id'),
+            'category_id': request.POST.get('category_id'),
+            'content': request.POST.get('content'),
+            'description': request.POST.get('description'),
+        }
+
+        if not all(value == None for value in data.values()):
+            Question.objects.create(**data)
+            return redirect('trivia_questions')
+
+        if question_id:
+            Question.objects.filter(pk=question_id).delete()
+            context['status'] = 'Categoría borrada con éxito'
+            return redirect('trivia_questions')
+
+    return render(request, 'core/admin/questions/index.html', context)
+
+@login_required
+def trivia_question_edit(request: HttpRequest, question_id: int):
+    context = {
+        'status': '',
+        'error': '',
+        'levels': Level.objects.all(),
+        'categories': Category.objects.all(),
+        'questions': Question.objects.all(),
+        'question_edit': Question.objects.get(pk=question_id),
+    }
+    if request.method == 'POST':
+        data = {
+            'level_id': request.POST.get('level_id'),
+            'category_id': request.POST.get('category_id'),
+            'content': request.POST.get('content'),
+            'description': request.POST.get('description'),
+        }
+
+        Question.objects.filter(pk=question_id).update(**data)
+
+        return redirect('trivia_questions')
+
+    return render(request, 'core/admin/questions/edit.html', context)
+
+@login_required
+def trivia_options(request: HttpRequest):
+    context = {
+        'status': '',
+        'error': '',
+        'levels': Level.objects.all(),
+        'options': Option.objects.all(),
+        'questions': Question.objects.all(),
+    }
+
+    if request.method == 'POST':
+        option_id = request.POST.get('option_id')
+
+        data = {
+            'level_id': request.POST.get('level_id'),
+            'question_id': request.POST.get('question_id'),
+            'content': request.POST.get('content'),
+            'isCorrect': None,
+        }
+
+        if not all(value is None for value in data.values()):
+            if request.POST.get('isCorrect') is 'on':
+                isCorrect = True
+            else:
+                isCorrect = False
+            data['isCorrect'] = isCorrect
+            Option.objects.create(**data)
+            return redirect('trivia_options')
+
+        if option_id:
+            Option.objects.filter(pk=option_id).delete()
+            context['status'] = 'Opción borrada con éxito.'
+            return redirect('trivia_options')
+
+    return render(request, 'core/admin/options/index.html', context)
+
+@login_required
+def trivia_option_edit(request: HttpRequest, option_id: int):
+    context = {
+        'status': '',
+        'error': '',
+        'levels': Level.objects.all(),
+        'options': Option.objects.all(),
+        'option_edit': Option.objects.get(pk=option_id),
+        'questions': Question.objects.all(),
+    }
+
+    if request.method == 'POST':
+        if request.POST.get('isCorrect') is 'on':
+            isCorrect = True
+        else:
+            isCorrect = False
+
+        data = {
+            'level_id': request.POST.get('level_id'),
+            'question_id': request.POST.get('question_id'),
+            'content': request.POST.get('content'),
+            'isCorrect': isCorrect,
+        }
+
+        data['isCorrect'] = isCorrect
+        Option.objects.filter(pk=option_id).update(**data)
+        return redirect('trivia_options')
+
+    return render(request, 'core/admin/options/edit.html', context)
