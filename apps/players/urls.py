a05382fdbd4245a0c1307ddfe710af0dce8c4ccb
IvanStacul <stacul.ivan98@gmail.com>
2021-09-03 10:48:17 -0300

    modificacion del flujo para el login y signup

--- apps/players/urls.py
+++ apps/players/urls.py
@@ -7,5 +7,4 @@ from . import views
 urlpatterns = [
     path('login', views.login_player, name='login'),
     path('logout', views.logout_player, name='logout'),
-    path('registo', views.signup_player, name='signup'),
 ]
