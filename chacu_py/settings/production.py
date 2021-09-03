48eb6582b7693b01aa688b1dfd0f4cab7e44a290
IvanStacul <stacul.ivan98@gmail.com>
2021-09-03 13:38:17 -0300

    heroku

--- chacu_py/settings/production.py
+++ chacu_py/settings/production.py
@@ -4,6 +4,12 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = ['chacu_py.herokuapp.com', ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # DATABASES = {
--- chacu_py/wsgi.py
+++ chacu_py/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chacu_py.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chacu_py.settings.production')
 
 application = get_wsgi_application()
