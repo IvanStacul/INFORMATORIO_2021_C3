40418e2fdd318dcc9539a84f185774874784861d
IvanStacul <stacul.ivan98@gmail.com>
2021-09-03 17:34:49 -0300

    DB update

--- chacu_py/settings/production.py
+++ chacu_py/settings/production.py
@@ -1,8 +1,9 @@
 from .base import *
+import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['chacu-py.herokuapp.com', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'chacu-py.herokuapp.com', ]
 
 DATABASES = {
     'default': {
@@ -10,6 +11,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # DATABASES = {
