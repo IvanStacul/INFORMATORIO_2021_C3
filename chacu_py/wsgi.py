ffd21d9b305b5fee6c196f1e49c2364d7a60744f
IvanStacul <stacul.ivan98@gmail.com>
2021-09-03 18:42:41 -0300

    .

--- chacu_py/wsgi.py
+++ chacu_py/wsgi.py
@@ -1,9 +1,8 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhitenoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chacu_py.settings.production')
 
 application = get_wsgi_application()
-application = DjangoWhitenoise(application)
+
